e4aa1b97df7a88155439183495c3761f82e00106
Molly Todd <mollyjt89@gmail.com>
2023-10-05 12:33:36 -0700

    completed all tests

--- server/app.py
+++ server/app.py
@@ -17,11 +17,46 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(
+            jsonify(response_dict_list),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'  # Set content type to JSON
+        return response
+    
+    def post(self):
+        data = request.json
+        name = data.get('name')
+        image = data.get('image')
+        price = data.get('price')
 
+        new_plant = Plant(name=name, image=image, price=price)
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+        response = make_response(
+            jsonify(response_dict),
+            201
+        )
+        response.headers['Content-Type'] = 'application/json'  # Set content type to JSON
+        return response
+    
+api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>') 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
